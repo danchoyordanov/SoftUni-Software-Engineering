7e5443124682d64fa1f1a1a543a150e6a4411a3f
danchoyordanov <dyordanov00@gmail.com>
2021-01-16 16:38:25 +0200

    committing task

---
+++ Java-Advanced/2MultidimensionalArrays/Lab/CompareMatrices.java
@@ -0,0 +1,69 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CompareMatrices {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[][] firstMatrix = readMatrix(scanner);
+        int[][] secondMatrix = readMatrix(scanner);
+
+        boolean areNotEqual = firstMatrix.length != secondMatrix.length;
+
+        if (!areNotEqual) {
+            for (int row = 0; row < firstMatrix.length; row++) {
+
+                int[] firstArr = firstMatrix[row];
+                int[] secondArr = secondMatrix[row];
+
+                areNotEqual = firstArr.length != secondArr.length;
+                if (!areNotEqual){
+                    for (int col = 0; col < firstArr.length ; col++) {
+                        if (firstArr[col] != secondArr[col]){
+                            areNotEqual = true;
+                            break;
+                        }
+                    }
+                }
+                if (areNotEqual){
+                    break;
+                }
+            }
+        }
+
+        String output = areNotEqual ? "not equal" : "equal";
+
+        System.out.println(output);
+
+    }
+
+    private static  int[][] readMatrix(Scanner scanner){
+
+        int[] rowsAndCols = readArray(scanner);
+        int rows = rowsAndCols[0];
+        int cols = rowsAndCols[1];
+
+        int[][] matrix= new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            matrix[i] = readArray(scanner);
+        }
+
+        return matrix;
+    }
+
+    private static void printMatrix(int[][] matrix) {
+        for (int row = 0; row < matrix.length ; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                System.out.print(matrix[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static int[] readArray(Scanner scanner) {
+        return Arrays.stream(scanner.nextLine().
+                split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+    }
+}
