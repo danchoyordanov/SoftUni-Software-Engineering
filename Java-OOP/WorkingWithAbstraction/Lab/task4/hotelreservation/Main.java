15d049780e71d51fcaaabbe02b1737f68c6ca25e
danchoyordanov <dyordanov00@gmail.com>
2021-02-24 15:45:25 +0200

    committing task

---
+++ Java-OOP/WorkingWithAbstraction/Lab/task4/hotelreservation/Main.java
@@ -0,0 +1,25 @@
+package hotelreservation;
+
+import IOUtils.ConsoleReader;
+import IOUtils.InputParser;
+
+public class Main {
+    public static void main(String[] args) {
+        runHoliday();
+    }
+
+    private static void runHoliday() {
+        ConsoleReader reader = new ConsoleReader();
+        String input = reader.readLine();
+        String[] tokens = InputParser.parseArray(input);
+
+        PriceCalculator calculator = new PriceCalculator(
+                Double.parseDouble(tokens[0]), Integer.parseInt(tokens[1]),
+                tokens[3].equals("SecondVisit") ? DiscountType.SECOND_VISIT
+                        : DiscountType.valueOf(tokens[3].toUpperCase()),
+                Season.valueOf(tokens[2].toUpperCase()));
+
+
+        System.out.printf("%.2f%n", calculator.calculate());
+    }
+}
