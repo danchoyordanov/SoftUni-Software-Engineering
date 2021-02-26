81fc6001f5ecaff210aa5afce1e3a4d230a73275
danchoyordanov <dyordanov00@gmail.com>
2021-02-27 01:16:01 +0200

    committing validation

---
+++ Java-OOP/2Encapsulation/Lab/validation/Person.java
@@ -0,0 +1,75 @@
+package validation;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double salary;
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public Person(String firstName, String lastName, int age, double salary) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setAge(age);
+        setSalary(salary);
+    }
+
+    public void setFirstName(String firstName) {
+        if (firstName.length() < 3) {
+            throw new IllegalArgumentException("First name cannot be less than 3 symbols");
+        }
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        if (lastName.length() < 3) {
+            throw new IllegalArgumentException("Last name cannot be less than 3 symbols");
+        }
+
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+
+        if (age <= 0) {
+            throw new IllegalArgumentException("Age cannot be zero or negative integer");
+        }
+        this.age = age;
+    }
+
+    private void setSalary(double salary) {
+        if (salary < 460.0) {
+            throw new IllegalArgumentException("Salary cannot be less than 460 leva");
+        }
+        this.salary = salary;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void increaseSalary(double percentage) {
+        if (this.age < 30) {
+            this.setSalary(this.getSalary() + (this.getSalary() * percentage / 100 / 2));
+        } else {
+            this.setSalary(this.getSalary() + (this.getSalary() * percentage / 100));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s gets " + this.getSalary() + " leva",
+                this.getFirstName(), this.getLastName());
+    }
+}
