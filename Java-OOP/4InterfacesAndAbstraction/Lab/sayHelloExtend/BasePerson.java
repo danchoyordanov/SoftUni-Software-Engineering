963be2d1d79e511d14e428c683ceb656687bb982
danchoyordanov <dyordanov00@gmail.com>
2021-03-06 14:38:43 +0200

    committing task 4 update of sayHello

---
+++ Java-OOP/4InterfacesAndAbstraction/Lab/sayHelloExtend/BasePerson.java
@@ -0,0 +1,17 @@
+package sayHelloExtend;
+
+public abstract class BasePerson implements Person {
+    private String name;
+    protected BasePerson(String name){
+        this.name = name;
+    }
+
+
+    public String getName(){
+        return name;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ Java-OOP/4InterfacesAndAbstraction/Lab/sayHelloExtend/Bulgarian.java
@@ -0,0 +1,12 @@
+package sayHelloExtend;
+
+public class Bulgarian extends BasePerson {
+    public Bulgarian(String name) {
+        super(name);
+    }
+
+    @Override
+    public String sayHello() {
+        return "Здравей";
+    }
+}
