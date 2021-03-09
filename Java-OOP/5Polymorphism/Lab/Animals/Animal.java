a6b3af775aaac1cd0421a94c1ac8d507a847196e
danchoyordanov <dyordanov00@gmail.com>
2021-03-09 23:24:50 +0200

    committing task

---
+++ Java-OOP/5Polymorphism/Lab/Animals/Animal.java
@@ -0,0 +1,13 @@
+package Animals;
+
+public abstract class Animal {
+    protected String name;
+    protected String favouriteFood;
+    public Animal(String name, String favouriteFood){
+        this.name = name;
+        this.favouriteFood = favouriteFood;
+
+    }
+
+    public abstract String explainSelf();
+}
